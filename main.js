534d22b9a5c841ff5a18276d38980dc33d4ef728
Possawat Tanam <possawat.tanam@mail.ac.th>
2021-02-28 13:04:02 +0700

    [add] clone gw4

---
+++ main.js
@@ -0,0 +1,112 @@
+const app = {
+  data() {
+    return {
+      Hokkaido: [{
+          name: 'Fort Goryokaku,Hakodate',
+          url: './images/1.jpg',
+          Like: false
+        },
+        {
+          name: 'Mount Hakodate,Hakodate',
+          url: './images/2.jpg',
+          Like: false
+        },
+        {
+          name: 'Otaru Canal Area,Otaru',
+          url: './images/3.jpg',
+          Like: false
+        },
+        {
+          name: 'Furano Flower Field,Furano',
+          url: './images/4.jpg',
+          Like: false
+        },
+        {
+          name: 'Furano Ski Area,Furano',
+          url: './images/5.jpg',
+          Like: false
+        },
+        {
+          name: 'Abashiri Drift Ice,Abashiri',
+          url: './images/6.jpg',
+          Like: false
+        },
+        {
+          name: 'Jigokudani,Noboribetsu',
+          url: './images/7.jpg',
+          Like: false
+        },
+        {
+          name: 'Aoiike Blue Pond,Biei',
+          url: './images/8.jpg',
+          Like: false
+        },
+        {
+          name: 'Asahiyama Zoo,Asahiyama',
+          url: './images/9.jpg',
+          Like: false
+        },
+        {
+          name: 'Shiroi Koibito,Sapporo',
+          url: './images/10.jpg',
+          Like: false
+        },
+        {
+          name: 'Sapporo TV Tower,Sapporo',
+          url: './images/11.jpg',
+          Like: false
+        },
+        {
+          name: 'Mount Moiwa,Sapporo',
+          url: './images/12.jpg',
+          Like: false
+        },
+      ],
+      inputsTask: '',
+      searchOpen: false,
+      searchFound: false,
+      selectImage: false,
+      currentIndex: 0
+    }
+  },
+  methods: {
+    likeCheck(index) {
+      this.Hokkaido[index].Like = !this.Hokkaido[index].Like
+    },
+    openSearchMenu() {
+      this.searchOpen = !this.searchOpen
+    },
+    closeSearchMenu() {
+      this.searchOpen = !this.searchOpen
+      this.inputsTask = ''
+    },
+    viewImg(index) {
+      this.selectImage = true;
+      this.currentIndex = index;
+    },
+    hideView() {
+      this.selectImage = false;
+    }
+  },
+
+  computed: {
+    sumLike() {
+      return this.Hokkaido.filter(t => t.Like).length
+    },
+    filteredList() {
+      this.searchFound = false
+      if (this.inputsTask == '') {
+        return this.Hokkaido
+      } else {
+        let search = this.Hokkaido.filter(t => this.inputsTask.toLowerCase().split(' ').every(v => t.name.toLowerCase().includes(v)))
+        if (search == '') {
+          this.searchFound = true
+        } else {
+          return search
+        }
+      }
+    }
+  }
+}
+
+mountedApp = Vue.createApp(app).mount('#app')
