bd14c419c23542a195b0f384b8ff1a7c3d9b786e
Possawat Tanam <possawat.tanam@mail.ac.th>
2021-02-28 14:19:44 +0700

    [edit] photoItem

--- components/PhotoItem.js
+++ components/PhotoItem.js
@@ -1,46 +1,44 @@
-app.component('photo-items',{
-    props:{
-      thaides:{
-        type: Array,
-        require:true
-      },
-      'search-found': Boolean,
-      'filtered-list': Array
-  
+app.component('photo-items', {
+  props: {
+    Hokkaido: {
+      type: Array,
+      require: true
     },
-  
-    /*html*/
-    template: 
-      `
-      <div class="grid grid-cols-3 auto-rows-auto gap-10 ">
-      <div v-for="(t,index) in filteredList" class="mx-auto">
-        <img :src="t.url" class="w-72 h-44 rounded-3xl" @click="showImg(index)">
-        <p class="text-center mt-4 text-xl select-none">
-          <i v-if="t.isLike" class="material-icons text-red-400 text-lg" v-on:dblclick="likeIt(index)">favorite</i>
-          <i v-else class="material-icons text-lg" v-on:dblclick="likeIt(index)">favorite_border</i>
-              {{t.name}}
-        </p>
+    'search-found': Boolean,
+    'filtered-list': Array
+
+  },
+  //html
+  template: `
+    <div class="grid grid-cols-3 auto-rows-auto gap-16 font-semibold">
+     <div v-for="(task,index) in filteredList" class="mx-auto">
+       <img :src="task.url" class="w-80 h-60 rounded-md border-2 border-red-400" v-on:click="viewImg(index)">
+         <p class="text-center mt-6 text-xl tracking-wider">
+           <i v-if="task.Like" class="material-icons text-red-400 text-4xl" v-on:click="likeCheck(index)">favorite</i>
+           <i v-else="task.Like" class="material-icons text-4xl" v-on:click="likeCheck(index)">favorite_border</i>
+           {{task.name}}
+         </p>
       </div>
-      <div v-if="searchFound" class="col-span-3 text-center mt-4 text-7xl">Photo Not Found !!!</div>
+
+      <div v-if="searchFound" class="text-center mt-48 text-9xl text-red-400">No Photo !!!</div>
     </div>
       `,
-        data(){
-          return {
-            showImages: false,
-            currentIndex: 0
-          }
-        },
-        methods: {
-          likeIt(index) {
-            this.currentIndex = index
-            this.$emit('like-it',this.currentIndex)
-          },
-          showImg(index) {
-            this.currentIndex = index
-            this.showImages = true
-            this.$emit('show-img', this.currentIndex)
-            this.$emit('show-images', this.showImages)
-          }
-        }
-    
-  })
+  data() {
+    return {
+      selectImage: false,
+      currentIndex: 0
+    }
+  },
+  methods: {
+    likeCheck(index) {
+      this.currentIndex = index
+      this.$emit('like-check', this.currentIndex)
+    },
+    viewImg(index) {
+      this.currentIndex = index
+      this.selectImage = true;
+      this.$emit('view-img', this.currentIndex)
+      this.$emit('view-images', this.selectImage)
+    },
+  }
+})
